4c0c34995f6930a22b361120663b831bd26476c4
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-17 16:20:56 +0300

    Modify

--- Music/app/controllers/album_controller.rb
+++ Music/app/controllers/album_controller.rb
@@ -1,7 +1,10 @@
 class AlbumController < ApplicationController
   def index
+      @albums = Album.all
+      render json: @albums
   end
 
   def show
+    @albums = Album.find(params[:id])
   end
 end
