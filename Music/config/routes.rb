ba11a6925846ddab8ff7b03de8bdc902f444e030
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-04-17 14:40:52 +0300

    DB , Models and controllers are created

--- Music/config/routes.rb
+++ Music/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'album/index'
+  get 'album/show'
+  get 'songs/index'
+  get 'songs/show'
+  get 'artists/index'
+  get 'artists/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
